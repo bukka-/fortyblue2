11db10ed05597ed8dbf0e376faf39e88826e8a9a
bukka- <bukka96@gmail.com>
2013-10-22 22:53:30 +0200

    Added drag functionality and small improvements

--- js/main.js
+++ js/main.js
@@ -1,30 +1,43 @@
 $(document).ready(function() {
 
+	function Draggable() {
+		$('.object').draggable({handle: '.drag', containment: ".canvas"});
+	};
+
 	function createObject(e) {
 		topPos = e.pageY - 10;
 		leftPos = e.pageX + 1;
-		console.log(topPos +',' + leftPos);
 
-		element = '<span class="object" id="'+object_id+'" style="top:'+topPos+'px; left:'+ leftPos+'px" contenteditable="true"></span>'
+		var source   = $("#te_object").html();
+		var element = Handlebars.compile(source);
+		var data = {
+			object_id: object_id,
+			topPos: topPos,
+			leftPos: leftPos
+		};
 
-		$('.objects').append(element);
-		$('#'+object_id).focus();
+		$('.objects').append(element(data));
+		$('#'+object_id+' .input-field').focus();
+		Draggable();
 		object_id++;
 	}
 
 	function preObject(e){
 		topPos = e.pageY - 10;
 		leftPos = e.pageX + 1;
-		console.log(topPos +',' + leftPos);
 
-		element = '<span class="pre-object-container" id="pre'+object_id+'" style="top:'+topPos+'px; left:'+ leftPos+'px"><span class="pre-object"></span></span>'
-
-		$('.objects').append(element);
+		var source   = $("#te_pre-object").html();
+		var element = Handlebars.compile(source);
+		var data = {
+			object_id: object_id,
+			topPos: topPos,
+			leftPos: leftPos
+		};
+		$('.objects').append(element(data));
 		$('#pre'+object_id+' .pre-object').animate({'width': '80px'}, 400);
 	}
 
-	object_id = 1;
-
+	var object_id = 1;
 	var pressTimer;
 	var press2Timer;
 
@@ -42,12 +55,14 @@ $(document).ready(function() {
 			$('#pre'+object_id).stop().remove();
 			createObject(e);
 		}, 500)
-
 		return true; 
 	});
 
-
-
+	$('body').on('mousedown','.drag', function(){
+		$(this).addClass('grabbing');
+	}).on('mouseup','.drag', function(){
+		$(this).removeClass('grabbing');
+	}); 
 
 
 });
